9621833b1677e85324980723181f93c7d7e35cb0
bouquet1 <bukuleta@gmail.com>
2023-10-07 14:23:39 -0700

    create empty migrations for starting database

---
+++ server/migrations/versions/f84e40cf060f_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: f84e40cf060f
+Revises: 
+Create Date: 2023-10-07 14:03:17.574931
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f84e40cf060f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
